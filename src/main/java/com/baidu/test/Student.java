17e039cc8c1e885961e81d1ff9be76a35f5bd15e
syxjava <10712829962@qq.com>
2020-03-22 20:05:53 +0800

    up

--- src/main/java/com/baidu/test/Student.java
+++ src/main/java/com/baidu/test/Student.java
@@ -2,6 +2,6 @@ package com.baidu.test;
 
 public class Student {
 	public static void main(String[] args){
-		System.out.println("left join! and sss");
+		System.out.println("up join! and sss");
 	}
 }
